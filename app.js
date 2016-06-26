5d5da9ae33eda1c2d6ad54efde33826fdd5caf95
Elliot Williams <elliot.james.williams@gmail.com>
2016-06-26 12:10:05 -0400

    API is final product

--- app.js
+++ app.js
@@ -1,47 +1,23 @@
 'use strict';
-//
-// var SwaggerExpress = require('swagger-express-mw');
-// var app = require('express')();
-// module.exports = app; // for testing
-//
-// var config = {
-//   appRoot: __dirname // required config
-// };
-//
-// SwaggerExpress.create(config, function(err, swaggerExpress) {
-//   if (err) { throw err; }
-//
-//   // install middleware
-//   swaggerExpress.register(app);
-//
-//   var port = process.env.PORT || 10010;
-//   app.listen(port);
-//
-//   if (swaggerExpress.runner.swagger.paths['/hello']) {
-//     console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
-//   }
-// });
 
-var twilio = require('twilio'),
-    http = require('http');
+var SwaggerExpress = require('swagger-express-mw');
+var app = require('express')();
+module.exports = app; // for testing
 
-http.createServer(function (req, res) {
-    // Create a TwiML response
-    console.log(req);
-    console.log(res);
-    var resp = new twilio.TwimlResponse();
-    console.log(resp);
+var config = {
+  appRoot: __dirname // required config
+};
 
-    // The TwiML response object will have functions on it that correspond
-    // to TwiML "verbs" and "nouns". This example uses the "Say" verb.
-    // Passing in a string argument sets the content of the XML tag.
-    // Passing in an object literal sets attributes on the XML tag.
-    // resp.say();
+SwaggerExpress.create(config, function(err, swaggerExpress) {
+  if (err) { throw err; }
 
-    //Render the TwiML document using "toString"
-    res.writeHead(200, {
-        'Content-Type':'text/xml'
-    });
-    res.end(resp.toString());
+  // install middleware
+  swaggerExpress.register(app);
 
-}).listen(process.env.VCAP_APP_PORT || 10010);
+  var port = process.env.PORT || 10010;
+  app.listen(port);
+
+  if (swaggerExpress.runner.swagger.paths['/hello']) {
+    console.log('try this:\ncurl http://127.0.0.1:' + port + '/hello?name=Scott');
+  }
+});
